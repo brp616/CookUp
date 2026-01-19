55e16f5425839dc3d429cba4d35786ca1551abb0
brp616 <brp616@gmail.com>
2026-01-19 13:14:31 -0500

    further package edits

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 import "./styles/variables.css";  // Import CSS variables
-import "./styles/layout.css";     // Import CSS layout
+import "./styles/Layout.css";     // Import CSS layout
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
