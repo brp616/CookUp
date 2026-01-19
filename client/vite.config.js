f83f60f415d119b68557f2b2e2d3f832734ceb80
brp616 <brp616@gmail.com>
2026-01-19 13:30:00 -0500

    allowing web host in preview for vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,5 +6,12 @@ export default defineConfig({
   plugins: [react()],
   allowedHosts: [
       'https://cookup-1gl6.onrender.com/'
-    ]
+    ],
+    preview: {
+    allowedHosts: ['cookup-1gl6.onrender.com']
+  },
+  // Just in case you also see this in development mode later:
+  server: {
+    allowedHosts: ['cookup-1gl6.onrender.com']
+  }
 })
